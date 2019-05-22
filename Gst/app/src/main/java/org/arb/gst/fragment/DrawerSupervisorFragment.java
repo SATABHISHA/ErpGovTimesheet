31a96e7845ee69a6381589f596d27f218af0696b
SATABHISHA <satabhisha.innita@gmail.com>
2019-05-22 12:43:02 +0530

    modified

--- Gst/app/src/main/java/org/arb/gst/fragment/DrawerSupervisorFragment.java
+++ Gst/app/src/main/java/org/arb/gst/fragment/DrawerSupervisorFragment.java
@@ -6,14 +6,94 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import org.arb.gst.R;
 
 public class DrawerSupervisorFragment extends Fragment {
     @Nullable
+    View rootView;
+    CheckBox checkbox_not_started,checkbox_saved,checkbox_correctionrequired,checkbox_posted,checkbox_submitted, checkbox_partiallyreturn,checkbox_approved;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_supervisor_drawer, container, false);
+        rootView =  inflater.inflate(R.layout.fragment_supervisor_drawer, null);
+        checkbox_not_started = rootView.findViewById(R.id.checkbox_not_started);
+        checkbox_saved = rootView.findViewById(R.id.checkbox_saved);
+        checkbox_correctionrequired = rootView.findViewById(R.id.checkbox_correctionrequired);
+        checkbox_posted = rootView.findViewById(R.id.checkbox_posted);
+        checkbox_submitted = rootView.findViewById(R.id.checkbox_submitted);
+        checkbox_partiallyreturn = rootView.findViewById(R.id.checkbox_partiallyreturn);
+        checkbox_approved = rootView.findViewById(R.id.checkbox_approved);
+
+//        onCheckboxClicked(rootView);
+        return rootView;
     }
+
+    /*private void onCheckboxClicked(View rootView) {
+        boolean checked = ((CheckBox) rootView).isChecked();
+        switch(rootView.getId()) {
+            case R.id.checkbox_not_started:
+                if (checked){
+                    Toast.makeText(getContext(),"Not Started",Toast.LENGTH_LONG).show();
+                }
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.checkbox_saved:
+                if (checked){
+
+                }
+                // Cheese me
+            else
+                // I'm lactose intolerant
+                break;
+            case R.id.checkbox_correctionrequired:
+                if (checked){
+
+                }
+                // Cheese me
+                else
+                    // I'm lactose intolerant
+                    break;
+            case R.id.checkbox_posted:
+                if (checked){
+
+                }
+                // Cheese me
+                else
+                    // I'm lactose intolerant
+                    break;
+            case R.id.checkbox_submitted:
+                if (checked){
+
+                }
+                // Cheese me
+                else
+                    // I'm lactose intolerant
+                    break;
+            case R.id.checkbox_partiallyreturn:
+                if (checked){
+
+                }
+                // Cheese me
+                else
+                    // I'm lactose intolerant
+                    break;
+            case R.id.checkbox_approved:
+                if (checked){
+
+                }
+                // Cheese me
+                else
+                    // I'm lactose intolerant
+                    break;
+
+            // TODO: Veggie sandwich
+        }
+    }*/
+
+
 }
